8796e6ab1a5f62066aea5c9a3449712569319365
Jesus Martin <Jesus.martin@clavisinsight.com>
2020-01-23 11:22:04 +0000

    skeloton

--- src/main/java/com/ascential/injesuswetrust/demo/controller/SearchController.java
+++ src/main/java/com/ascential/injesuswetrust/demo/controller/SearchController.java
@@ -1,7 +1,9 @@
 package com.ascential.injesuswetrust.demo.controller;
 
+import com.ascential.injesuswetrust.demo.model.SearchTermResult;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -10,21 +12,23 @@ import java.util.List;
 @RestController("/api/search")
 public class SearchController {
 
+    @GetMapping("country/list")
     public ResponseEntity<List<String>> getCountries(@Param("global_customer_id") Integer globalCustomerId,
         @Param("report_date") LocalDate reportDate) {
 
 
 
-        return ResponseEnt.ok()
+        return ResponseEntity.ok(null);
 
     }
+    @GetMapping("translator")
 
-    public ResponseEntity<List<Search>> getSearchTerms(@Param("global_customer_id") Integer globalCustomerId,
-                                                     @Param("report_date") LocalDate reportDate) {
+    public ResponseEntity<List<SearchTermResult>> getSearchTerms(@Param("global_customer_id") Integer globalCustomerId,
+                                                                 @Param("report_date") LocalDate reportDate, @Param("country_code") String countryCode) {
 
 
 
-        return ResponseEnt.ok()
+        return ResponseEntity.ok(null);
 
     }
 }
--- src/main/java/com/ascential/injesuswetrust/demo/model/SearchTermResult.java
+++ src/main/java/com/ascential/injesuswetrust/demo/model/SearchTermResult.java
@@ -1,4 +1,16 @@
 package com.ascential.injesuswetrust.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchTermResult {
+
+    private String source;
+    private String target;
+    private int score;
+
 }
