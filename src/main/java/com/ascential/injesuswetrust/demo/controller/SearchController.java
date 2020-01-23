8ffc0235a159e21b268ad53a929be78bc24099b8
Cesar Venancio <cesar.venancio@clavistechnology.com>
2020-01-23 14:32:25 +0000

    Removing useless endpoint

--- src/main/java/com/ascential/injesuswetrust/demo/controller/SearchController.java
+++ src/main/java/com/ascential/injesuswetrust/demo/controller/SearchController.java
@@ -66,20 +66,5 @@ public class SearchController {
 
         //return ResponseEntity.ok(result);
     }
-
-    @ApiOperation(value = "Get a list of original and translated search terms with their related score", nickname = "getTranslatedSearchTerms")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success", response = List.class),
-            @ApiResponse(code = 204, message = "No Content"),
-            @ApiResponse(code = 401, message = "Unauthorized"),
-            @ApiResponse(code = 403, message = "Forbidden"),
-            @ApiResponse(code = 404, message = "Not Found"),
-            @ApiResponse(code = 500, message = "Failure"),
-            @ApiResponse(code = 400, message = "Bad Request")})
-    @GetMapping("searchDao")
-    public ResponseEntity<List<SearchTermResult>> getSearchTermsDao(@Param("global_customer_id") Integer globalCustomerId,
-                                                              @Param("report_date") LocalDate reportDate, @Param("country_code") String countryCode) {
-        List<SearchTermResult> result = searchService.getSearchTermResults(1, LocalDate.now(), "UK");
-        return ResponseEntity.ok(result);
-    }
+    
 }
